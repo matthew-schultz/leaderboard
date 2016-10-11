3fb107c7d6bf739275aa5ce1920d36e3c6b8a6a7
matthew-schultz <mschultz@hawaii.edu>
2016-10-11 10:20:33 -1000

    Pre: 5

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -2,3 +2,28 @@
  * Created by matthew on 10/11/2016.
  */
 
+PlayersList = new Mongo.Collection('players');
+
+console.log("asdf");
+
+if(Meteor.isClient){
+  // this code only runs on the client
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    },
+    'otherHelperFunction': function(){
+      return "Some other function";
+    }
+  });
+
+  Template.leaderboard.events({
+      'click .player': function(){
+        console.log("You clicked a .player element");
+      }
+  });
+}
+
+if(Meteor.isServer){
+
+}
