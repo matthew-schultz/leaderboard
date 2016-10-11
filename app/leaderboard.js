f007efea4ab8f198a062113fcac106c901f21f27
matthew-schultz <mschultz@hawaii.edu>
2016-10-11 10:40:04 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -14,12 +14,23 @@ if(Meteor.isClient){
     },
     'otherHelperFunction': function(){
       return "Some other function";
+    },
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected"
+      }
     }
   });
 
   Template.leaderboard.events({
       'click .player': function(){
-        console.log("You clicked a .player element");
+        //console.log("You clicked a .player element");
+        var playerId = this._id;
+        Session.set('selectedPlayer', playerId);
+        var selectedPlayer = Session.get('selectedPlayer');
+        console.log(selectedPlayer);
       }
   });
 }
